c27be7e658ac059726d3697927bf66d081e6045c
kickeyhickey <irishmanhickey@gmail.com>
2022-02-10 13:56:08 -0800

    nav complete

---
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+export default class ApartmentIndex extends Component {
+  render() {
+    return (
+      <>
+        <h3>Index</h3>
+      
+      </>
+    )
+  }
+}
---
+++ app/javascript/mockApartments.js
@@ -0,0 +1,43 @@
+let apartments = [
+  {
+    user_id: 1,
+    pets: "cats only",
+    bathrooms: 3,
+    bedrooms: 3,
+    price: "2500/month",
+    email: "rabble@dabble.com" ,
+    manager: "Hickey" ,
+    state: "CA" ,
+    city: "San Diego" ,
+    street:  "Hickey blvd, 92114"
+
+  },
+  {
+    user_id: 1,
+    pets: "cats only",
+    bathrooms: 1,
+    bedrooms: 1,
+    price: "1400/month",
+    email: "rabble@dabble.com" ,
+    manager: "Hickey" ,
+    state: "CA" ,
+    city: "San Diego" ,
+    street:  "Ocean ave, 92116"
+  },
+  {
+    
+      user_id: 1,
+      pets: "cats only",
+      bathrooms: 4,
+      bedrooms: 1,
+      price: "4000/month",
+      email: "rabble@dabble.com" ,
+      manager: "Hickey" ,
+      state: "CA" ,
+      city: "San Diego" ,
+      street:  "TooRich blvd, 87443"
+    
+  }
+  
+]
+export default apartments
